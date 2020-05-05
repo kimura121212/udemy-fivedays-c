1eb3c977bd6ab97cd1ca2877b6a8dfe2a9bfb2d1
kimura121212 <ykimuranoi@gmail.com>
2020-05-05 13:08:39 +0900

    triangle

---
+++ triangle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int base, height;
+    int area;
+
+    base = 3;
+    height = 4;
+
+    area = base * height / 2;
+    printf("triangle area %d\n", area);
+
+    return 0;
+}
