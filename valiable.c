ca8434ee9d5e38b3a431a9e3a013650ebcdb16c7
kimura121212 <ykimuranoi@gmail.com>
2020-05-05 13:06:42 +0900

    valiable

---
+++ valiable.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (){
+    int a, c;
+
+    a = 10;
+
+    c = a;
+    for (int i = 0; i < 10; i++) {
+        printf("aaa\n");
+    }
+
+    int b;
+    b = 20;
+
+    printf ("%d\n",b);
+    return 0;
+}
