bd893770961ceb05bb99cfa039d8dd822c961366
kimura121212 <ykimuranoi@gmail.com>
2020-05-05 13:29:18 +0900

    while scanf

--- while.c
+++ while.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main () {
     int i=0;
 
-    while (i<10){
-        printf("Hello %d\n", i);
-        i++;
+    while (true){
+        scanf("%d", &i);
+
+        if (i==0){
+            break;
+        }
+        printf("Your input number is %d\n", i);
+
     }
 
     return 0;
